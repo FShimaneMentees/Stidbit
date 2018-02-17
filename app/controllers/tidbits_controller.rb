ef4919601dc0b94bd36a10e050f364166130b4df
ayumu taniguchi <a.taniguchi@fenrir.co.jp>
2018-02-17 12:35:20 +0900

    add: 新規投稿ページの作成

--- app/controllers/tidbits_controller.rb
+++ app/controllers/tidbits_controller.rb
@@ -2,4 +2,7 @@ class TidbitsController < ApplicationController
   def index
     @tidbits = Tidbit.all
   end
+
+  def new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'tidbits/index'
 
+  get '/new' => 'tidbits#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
