0324a05e84c27735b3818733d3526e5f79f17fd4
ayumu taniguchi <a.taniguchi@fenrir.co.jp>
2018-02-17 12:38:00 +0900

    modify: 記事一覧画面のルーティングをルートパスに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'tidbits/index'
-
+  get '/' => 'tidbits#index'
   get '/new' => 'tidbits#new'
   post '/create' => 'tidbits#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
