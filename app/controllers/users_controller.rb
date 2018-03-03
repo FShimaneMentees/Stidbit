7416a7ba23ddde1d770f4c6a959d76df2a84a8fd
ha-inoue <ha.inoue@fenrir.co.jp>
2018-03-03 16:21:58 +0900

    add: ログイン・ログアウト機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,4 +16,28 @@ class UsersController < ApplicationController
       render('new')
     end
   end
+
+  def login_form; end
+
+  def login
+    @user = User.find_by(
+      email: params[:email]
+    )
+    if @user&.authenticate(params[:password])
+      session[:user_id] = @user.id
+      flash[:notice] = 'ログインしました'
+      redirect_to('/')
+    else
+      @message = 'メールアドレスまたはパスワードが間違っています。'
+      @email = params[:email]
+      @password = params[:password]
+      render('login_form')
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = 'ログアウトしました'
+    redirect_to('/login')
+  end
 end
