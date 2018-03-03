532c709a00425be172d5ae6eef80a7ff3196585a
ayumu taniguchi <a.taniguchi@fenrir.co.jp>
2018-03-03 11:44:38 +0900

    modify[Userモデル]: nameカラムをユニークに修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :name, presence: true, length: { maximum: 10 }
+  validates :name, presence: true, uniqueness: true, length: { maximum: 10 }
   validates :email, presence: true, uniqueness: true
 end
