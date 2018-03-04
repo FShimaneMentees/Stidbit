df9493b46af44fe3c08f3fed61d0443ac0537d74
ha-inoue <ha.inoue@fenrir.co.jp>
2018-03-04 14:56:35 +0900

    add: ログインユーザー判定用のアクション名を変更する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :set_login_user
+  before_action :set_current_user
 
-  def set_login_user
+  def set_current_user
     @login_user = User.find_by(id: session[:user_id])
   end
 
