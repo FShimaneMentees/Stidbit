d1af511628a6861e8910f243dd5d1a868f4d8a95
ha-inoue <ha.inoue@fenrir.co.jp>
2018-03-04 17:07:11 +0900

    modify: ログインユーザーの判定及び取得処理をヘルパーにする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :set_current_user
+  before_action :current_user
+  helper_method :current_user, :user_signed_in?
 
-  def set_current_user
-    @login_user = User.find_by(id: session[:user_id])
+  private 
+  
+  def current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
+  
+  def user_signed_in?
+    @current_user != nil
   end
 
   def authenticate_user
-    return if @login_user
+    return if user_signed_in?
 
     flash[:notice] = 'ログインが必要です'
     redirect_to('/login')
   end
 
-  def forbid_login_user
-    return unless @login_user
+  def forbid_current_user
+    return unless user_signed_in?
 
     flash[:notice] = 'すでにログインしています'
     redirect_to('/')
