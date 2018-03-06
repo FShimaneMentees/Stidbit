05669ee1b752e5f0b67cfdc0003eae312d76feb9
ayumu taniguchi <a.taniguchi@fenrir.co.jp>
2018-03-06 21:23:11 +0900

    add: migrationファイルの追加

--- app/models/tidbit.rb
+++ app/models/tidbit.rb
@@ -1,4 +1,11 @@
 class Tidbit < ApplicationRecord
+  belongs_to :user
+  has_many :likes, dependent: :destroy
+  
   validates :title, presence: true, length: { maximum: 30 }
   validates :content, presence: true, length: { maximum: 200 }
+
+  def like_user(user_id)
+    likes.find_by(user_id: user_id)
+  end
 end
