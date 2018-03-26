fbcd3ae52961c6cff88e068634d961245356512a
ha-inoue <ha.inoue@fenrir.co.jp>
2018-03-26 18:43:52 +0900

    modify: user_id の nil を許可するオプションを外す

--- app/models/tidbit.rb
+++ app/models/tidbit.rb
@@ -1,5 +1,5 @@
 class Tidbit < ApplicationRecord
-  belongs_to :user, optional: true
+  belongs_to :user
   has_many :likes, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 30 }
